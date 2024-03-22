10aefbe19a03509d340f165c1f6959815744343e
shassaan <hassaan.ahmed@sadapay.pk>
2024-03-23 02:30:40 +0500

    Bump version: 1.0.0 → 2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dbt-redshift-dryrun',
-    version='1.0.0',  
+    version='2.0.0',  
     scripts="cli/app.py",
     description='Python package dry running redshift queries',
     url='https://github.com/shassaan/dbt-redshift-dryrun',
